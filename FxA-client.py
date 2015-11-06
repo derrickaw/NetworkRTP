89ac98bb9f119619f6330c7ef4eb7609b1f07a2c
R. Elliott Childre <rchildre3@gatech.edu>
2015-11-05 20:27:07 -0500

    client parameters set up

--- FxA-client.py
+++ FxA-client.py
@@ -3,25 +3,42 @@ import socket
 
 
 def main(argv):
-    isConnected = False
-    clientport = ''
-    ipaddress = ''
-    netemuport = ''
-    x = ''
     if len(argv) != 3:
         print "Correct usage: FxA-Client X A P"
         sys.exit(1)
-    if int(argv[0]) % 2 == 1:
-        print()
-    clientport = int(argv[0])
+
+    clientport = argv[0]
     ipaddress = argv[1]
     netemuport = argv[2]
+    isConnected = False
+    x = ''
+
     try:
-        socket.inet_aton(ipaddress)
-    except socket.error:
-        print "Invalid IP notation in param A"
+        clientport = int(clientport)
+    except ValueError:
+        print('Invalid client port number %s' % argv[0])
         sys.exit(1)
-    # TODO check if port is open!
+
+    if clientport % 2 == 1:
+        print('Client port number: %d was not even number' % clientport)
+        sys.exit(1)
+
+    try:
+        ipaddress = socket.inet_aton(ipaddress)
+    except socket.error:
+        print("Invalid IP notation: %s" % argv[1])
+        sys.exit(1)
+        # TODO check if port is open!
+
+    try:
+        netemuport = int(netemuport)
+    except ValueError:
+        print('Invalid NetEmu port number: %s' % argv[2])
+        sys.exit(1)
+
+    # print 'print type(clientport)' + str(type(clientport))
+    # print 'print type(ipaddress)' + str(type(ipaddress))
+    # print 'print type(netemuport)' + str(type(netemuport))
 
     print('Command Options:')
     print('connect\t\t|\tConnects to the FxA-server')
@@ -40,29 +57,35 @@ def main(argv):
             break
         else:
             y = x.split(" ")
-            if len(y) != 2:
-                print("Invalid command: get, post, window require secondary parameter")
-                break
             if y[0] == 'get':
+                if len(y) != 2:
+                    print("Invalid command: get requires secondary parameter")
+                    continue
                 if isConnected:
                     # TODO get()
                     print('get')
                 else:
                     print('get not valid without existing connection')
             elif y[0] == 'post':
+                if len(y) != 2:
+                    print("Invalid command: post requires secondary parameter")
+                    continue
                 if isConnected:
                     # TODO post()
                     print('post')
                 else:
                     print('post not valid without existing connection')
             elif y[0] == 'window':
+                if len(y) != 2:
+                    print("Invalid command: window requires secondary parameter")
+                    continue
                 if isConnected:
                     # TODO window()
                     print('window')
                 else:
                     print('window not valid without existing connection')
             else:
-                print("Invalid command")
+                print("Command not recognized")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
